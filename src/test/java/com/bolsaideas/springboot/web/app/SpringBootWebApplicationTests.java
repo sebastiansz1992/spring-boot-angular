5e0158968ba32f1baa34f9e3f0f9eb5f68779d8a
Sebastian Agudelo Marin <sebastian.agudelo@accenture.com>
2021-04-20 21:48:10 -0500

    Proyecto base

---
+++ src/test/java/com/bolsaideas/springboot/web/app/SpringBootWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bolsaideas.springboot.web.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
