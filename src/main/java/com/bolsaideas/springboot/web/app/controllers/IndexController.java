f2f1bd7e8b4e4fdc9c9943ce3d6fcbaf6f2b8901
Sebastian Agudelo Marin <seagude@bancolombia.com.co>
2021-04-22 17:25:56 -0500

    feat: se adiciona el template listar.html

--- src/main/java/com/bolsaideas/springboot/web/app/controllers/IndexController.java
+++ src/main/java/com/bolsaideas/springboot/web/app/controllers/IndexController.java
@@ -6,6 +6,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 @Controller
 @RequestMapping("/app")
 public class IndexController {
@@ -22,10 +26,21 @@ public class IndexController {
         Usuario usuario = new Usuario();
         usuario.setNombre("Sebastian");
         usuario.setApellido("Agudelo Marin");
-
+        usuario.setEmail("sebascarman@gmail.com");
         model.addAttribute("usuario", usuario);
         model.addAttribute("titulo", "Perfil del usuario: ".concat(usuario.getNombre()));
 
         return "perfil";
+    }
+
+    @GetMapping("/listar")
+    public String listar(Model model) {
+        List<Usuario> usuarios = Arrays.asList(new Usuario("Sebastian", "Agudelo Marin", "sebascarman@gmail.com"),
+                new Usuario("John", "Doe", "john@gmail.com"),
+                new Usuario("Jane", "Doe", "jane@gmail.com"),
+                new Usuario("Dani", "Ayala", "dani@gmail.com"));
+        model.addAttribute("titulo", "Listado de usuarios");
+        model.addAttribute("usuarios", usuarios);
+        return "listar";
     }
 }
