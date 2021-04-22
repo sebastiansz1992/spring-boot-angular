2aeb8a905e23eed57b6a2e1a56b53aa777cbae53
Sebastian Agudelo Marin <seagude@bancolombia.com.co>
2021-04-21 22:00:21 -0500

    se adiciona el controlador y la interfaz usuario

--- src/main/java/com/bolsaideas/springboot/web/app/controllers/IndexController.java
+++ src/main/java/com/bolsaideas/springboot/web/app/controllers/IndexController.java
@@ -1,15 +1,31 @@
 package com.bolsaideas.springboot.web.app.controllers;
 
+import com.bolsaideas.springboot.web.app.models.Usuario;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/app")
 public class IndexController {
 
     @GetMapping({"/index", "/", "/home"})
     public String index(Model model) {
         model.addAttribute("titulo", "Hola Spring Framework Model!");
         return "index";
+    }
+
+    @GetMapping("/perfil")
+    public String perfil(Model model) {
+
+        Usuario usuario = new Usuario();
+        usuario.setNombre("Sebastian");
+        usuario.setApellido("Agudelo Marin");
+
+        model.addAttribute("usuario", usuario);
+        model.addAttribute("titulo", "Perfil del usuario: ".concat(usuario.getNombre()));
+
+        return "perfil";
     }
 }
